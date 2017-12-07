c3515206e101f4f8b483a26dbf73dca19e2ff119
Darice <darice.guittet@nrel.gov>
2017-12-06 20:38:23 -0700

    Revert "merge part1 writeup"
    This reverts commit 92ba5f6240ab7860a8e816bd045601ccf42c6214, reversing
    changes made to 8b3f2e7b2bd837ee260fb86dc9d5f15ee9ec70e9.

--- Extra_credit_one_attempt_two.m
+++ Extra_credit_one_attempt_two.m
@@ -1,13 +1,13 @@
 %%% Initialize variables
-dx = 1; %change in x
-Nx = 20/dx; % Choose the number of x steps 
-dt = 0.01; %Length of each time step in seconds 
+dx = .1; %change in x
+Nx = 200; % Choose the number of x steps 
+dt = 0.1; %Length of each time step in seconds 
 Nt = 50/dt; % Choose the number of time steps
 b=dt/(dx*dx);
 
 
 V = zeros(Nx+1,Nt+1);
-for i=1:Nx+1
+for i=1:201
 V(i, 1)=InitialData(i*dx-10.1);
 end
 
